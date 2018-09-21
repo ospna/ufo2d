020473f8b5d74058287b037ab945e650667b684f
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2018-09-21 16:38:20 -0400

    ufo

---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody2D rgb2d;
+    private int count;
+
+    void Start()
+    {
+        rgb2d = GetComponent<Rigidbody2D> ();
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rgb2d.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.CompareTag("PickUp"))
+        {
+            //... then set the other object we just collided with to inactive.
+            coll.gameObject.SetActive(false);
+
+            //Add one to the current value of our count variable.
+            count = count + 1;
+
+            //Update the currently displayed count by calling the SetCountText function.
+            SetCountText();
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
